ede1b0a3315eebe696536511fa6226bc600a1c2d
dflong <1197083642@qq.com>
2025-09-02 09:18:58 +0800

    init

---
+++ src/main/java/com/dflong/kapu/KapuApplication.java
@@ -0,0 +1,12 @@
+package com.dflong.kapu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KapuApplication.class, args);
+    }
+}
